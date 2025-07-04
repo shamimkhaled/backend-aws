fc4d410299a247eb7478568362b5ef88c69aba8f
Shamim Khaled <i.amkhaled89@gmail.com>
2025-07-04 23:01:03 +0600

    vercel

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,11 @@
 
 echo "📦 Installing dependencies..."
-pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 echo "📁 Applying migrations..."
-python3.11 manage.py migrate
+python3.9 manage.py migrate
 
 echo "🎨 Collecting static files..."
-python3.11 manage.py collectstatic --no-input --clear
+python3.9 manage.py collectstatic --no-input --clear
 
 echo "✅ Build completed!"
