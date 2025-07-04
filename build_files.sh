ddb5b01605d16d489cd4acb5a2eb71dbe796d518
Shamim Khaled <i.amkhaled89@gmail.com>
2025-07-04 22:49:45 +0600

    update

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,11 @@
 
-# build_files.sh
-
+echo "📦 Installing dependencies..."
 pip install -r requirements.txt
+
+echo "📁 Applying migrations..."
+python3.11 manage.py migrate
+
+echo "🎨 Collecting static files..."
 python3.11 manage.py collectstatic --no-input --clear
+
+echo "✅ Build completed!"
