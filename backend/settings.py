284dc8d1e7be4482f8a74afc3dafb39cfb17cfec
Shamim Khaled <i.amkhaled89@gmail.com>
2025-07-04 22:19:06 +0600

    settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pxhv-$!_6%&zgw%tq3wexeyb1@2yrnb0(=ibb)*0m61xtjwa3&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
