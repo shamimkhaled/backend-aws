9ca46139e9ef9b67c9c63a7c7a2a1929ac66518e
Shamim Khaled <i.amkhaled89@gmail.com>
2025-07-10 12:46:05 +0600

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,13 @@
 
+
 echo "📦 Installing dependencies..."
 python3.9 -m pip install -r requirements.txt
 
+echo "📁 Creating staticfiles directory..."
+mkdir -p staticfiles
+
 echo "📁 Applying migrations..."
-python3.9 manage.py migrate
+python3.9 manage.py migrate --run-syncdb
 
 echo "🎨 Collecting static files..."
 python3.9 manage.py collectstatic --no-input --clear
